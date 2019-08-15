2665a86115bf4c7ad374d38ef7ceeb769d2df101
m-yoshiro <massugu.massugu@gmail.com>
2019-08-15 13:49:56 +0900

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const env = process.env.NODE_ENV;
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
